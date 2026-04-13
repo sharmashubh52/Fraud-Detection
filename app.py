77cadc2e52a47bb1453ab19744e68dfeb46af1c1
sharmashubh52 <shubhgt19082@gmail.com>
2026-04-13 18:13:23 +0530

    added backend exception visibility

--- app.py
+++ app.py
@@ -1,3 +1,36 @@
+#from flask import Flask, request, jsonify
+#from model.predict import predict_transaction
+#from utils.feature_builder import build_feature_vector
+#from utils.db import transactions_collection
+#import os
+
+#app = Flask(__name__)
+
+#@app.route("/")
+#def home():
+#    return jsonify({"message": "Fraud Detection API Running"})
+
+#@app.route("/check_transaction", methods=["POST"])
+#def check_transaction():
+#    data = request.json
+
+#    features = build_feature_vector(data)
+#    prediction, risk_score = predict_transaction(features)
+
+#    record = data.copy()
+#    record["prediction"] = prediction
+#    record["risk_score"] = risk_score
+
+#    transactions_collection.insert_one(record)
+
+#    return jsonify({
+#        "prediction": prediction,
+#        "risk_score": risk_score
+#    })
+
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 5000))
+#    app.run(host="0.0.0.0", port=port)
 from flask import Flask, request, jsonify
 from model.predict import predict_transaction
 from utils.feature_builder import build_feature_vector
@@ -10,23 +43,32 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Fraud Detection API Running"})
 
+
 @app.route("/check_transaction", methods=["POST"])
 def check_transaction():
-    data = request.json
+    try:
+        data = request.get_json()
 
-    features = build_feature_vector(data)
-    prediction, risk_score = predict_transaction(features)
+        features = build_feature_vector(data)
+        prediction, risk_score = predict_transaction(features)
 
-    record = data.copy()
-    record["prediction"] = prediction
-    record["risk_score"] = risk_score
+        record = data.copy()
+        record["prediction"] = prediction
+        record["risk_score"] = risk_score
 
-    transactions_collection.insert_one(record)
+        # Mongo save
+        transactions_collection.insert_one(record)
 
-    return jsonify({
-        "prediction": prediction,
-        "risk_score": risk_score
-    })
+        return jsonify({
+            "prediction": prediction,
+            "risk_score": risk_score
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
