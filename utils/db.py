74a4613ed46e50422bd47950b274b4459337a51d
Shubh Sharma <choudhary0030430@gmail.com>
2026-04-13 15:58:14 +0530

    render backend deployment ready

--- utils/db.py
+++ utils/db.py
@@ -1,11 +1,22 @@
-from pymongo import MongoClient
+#from pymongo import MongoClient
 
 # Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/")
+#client = MongoClient("mongodb://localhost:27017/")
 
 # Create database
-db = client["fraud_detection_db"]
+#db = client["fraud_detection_db"]
 
 # Create collection
-transactions_collection = db["transactions"]
+#transactions_collection = db["transactions"]
 
+from pymongo import MongoClient
+import os
+
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb+srv://labhvaidh_db_user:rxbh2xoA0E8V5NJd@cluster0.cawva5c.mongodb.net/?appName=Cluster0"
+)
+
+client = MongoClient(MONGO_URI)
+db = client["fraud_detection_db"]
+transactions_collection = db["transactions"]
